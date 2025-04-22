29cb87d131758215f8ddfc8f3343d8da70254430
Kai Wang <kaiuchihaa@gmail.com>
2025-04-22 11:38:52 -0300

    iniciado o curso de flutter com conexão a API usando json-server

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:my_frist_flutter_project/data/task_inherited.dart';
 import 'package:my_frist_flutter_project/screens/form_screen.dart';
 import 'package:my_frist_flutter_project/screens/initial_screen.dart';
@@ -16,20 +17,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Kodersolutions flutter app.',
       theme: ThemeData(
+        primarySwatch: Colors.grey,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: AppBarTheme(
+          color: Colors.pink,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 22),
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.white, size:30)
+        ),
+        textTheme: GoogleFonts.bitterTextTheme()
       ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
       // home: TaskInherited(child: InitialScreen()),
       initialRoute: "home",
       routes: {
-        "home": (context) => TaskInherited(child: InitialScreen(),),
+        "home": (context) => TaskInherited(child: InitialScreen()),
         "form-screen": (context) => FormScreen(taskContext: context),
-
       },
       //home: FormScreen()
     );
   }
 }
-
-
-
-
